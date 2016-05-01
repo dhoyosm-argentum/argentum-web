932ad109bb1b52447568df7c2479c836aef17148
Daniel Hoyos <dhoyosm@gmail.com>
2016-05-01 18:07:10 -0500

    scaffolds the AngularJS web app, using Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name argentumWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the argentumWebApp
+ */
+angular.module('argentumWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name argentumWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the argentumWebApp
+ */
+angular.module('argentumWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
